e405c6c224d5e0aa0e5cafb58a8837295757e03e
unknown <bugboogie666@gmail.com>
2020-01-26 20:37:27 +0100

    My solution of exercise 2

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private List<Customer> customers = new List<Customer>
+        {
+                new Customer {Id = 1, Name = "John Smith"},
+                new Customer {Id = 2, Name = "Mary Williams" }
+        };
+
+
+        // GET: Customers
+        [Route("Customers")]
+        public ActionResult Index()
+        {
+            RandomMovieViewModel viewModel = new RandomMovieViewModel()
+            {
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+        [Route("Customers/detail/{id:range(1,2)}")]
+        public ActionResult Details(int id)
+        {
+            RandomMovieViewModel viewModel = new RandomMovieViewModel()
+            {
+                Customer = customers[id - 1]
+            };
+            return View(viewModel);
+        }
+    }
+}
